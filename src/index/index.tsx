90d9473f73a7e4aee8b4d30e8ef7c3d09eb86c28
z@wood <2511841570@qq.com>
2020-08-18 08:25:18 +0800

    :art: 改进index代码结构

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -2,6 +2,34 @@ import React, {useState} from 'react';
 import './index.scss';
 import {Link} from 'react-router-dom';
 import Loading from '../components/loading';
+
+const Words: React.FC = () => {
+  return (
+    <div className="index-words">
+      <div id="sentence1">
+        你即将看到的是<span style={{ fontSize: "6.5vw" }}>家园扭蛋机</span>
+        <p style={{ fontSize: "6.3vw" }}>NCUHOME’S GACHAPON</p>
+      </div>
+      <div id="sentence2">
+        正如它的英文名 (咔擦砰)
+        <p><span style={{ fontSize: "8vw", marginLeft: 20 }}>"咔擦"</span>一声</p>
+      </div>
+      <div id="sentence3">
+        <p>2019年</p>
+        <p>那些或许被你忽视的小碎片</p>
+        <p>就会<span style={{ fontSize: "8vw" }}>"砰"</span>的涌现</p>
+        <p>月光下的足迹</p>
+        <p>教室里的身影</p>
+        <p>一些无意坚持下来的小习惯...</p>
+      </div>
+      <div id="sentence4">
+        <p>把它们收集起来</p>
+        <p>一起来拼出你的校园人设</p>
+        <p style={{ fontSize: "6.5vw" }}>惊喜在这里等你...</p>
+      </div>
+    </div>
+  )
+}
 const Index: React.FC = () => {
   const [flag, setFlag] = useState<boolean>(true);
   const [show, changeShow] = useState<boolean>(false);
@@ -54,7 +82,7 @@ const Index: React.FC = () => {
   }
   return (
     <div>
-      <div id="loading"><Loading /></div>
+      {/* <div id="loading"><Loading /></div> */}
       <div className="index-container" id="container" onClick={toBottom}>
         <div className="index-title">2019
           <span className="index-flag">
@@ -62,29 +90,7 @@ const Index: React.FC = () => {
           </span>
           <div style={{fontSize: "6vw"}}>(任意点击进入扭蛋机)</div>
         </div>
-        <div className="index-words">
-          <div id="sentence1">
-            你即将看到的是<span style={{ fontSize: "6.5vw" }}>家园扭蛋机</span>
-            <p style={{ fontSize: "6.3vw" }}>NCUHOME’S GACHAPON</p>
-          </div>
-          <div id="sentence2">
-            正如它的英文名 (咔擦砰)
-            <p><span style={{ fontSize: "8vw", marginLeft: 20 }}>"咔擦"</span>一声</p>
-          </div>
-          <div id="sentence3">
-            <p>2019年</p>
-            <p>那些或许被你忽视的小碎片</p>
-            <p>就会<span style={{ fontSize: "8vw" }}>"砰"</span>的涌现</p>
-            <p>月光下的足迹</p>
-            <p>教室里的身影</p>
-            <p>一些无意坚持下来的小习惯...</p>
-          </div>
-          <div id="sentence4">
-            <p>把它们收集起来</p>
-            <p>一起来拼出你的校园人设</p>
-            <p style={{ fontSize: "6.5vw" }}>惊喜在这里等你...</p>
-          </div>
-        </div>
+        <Words />
         {show ? (<Link to="/papers" className="index-get">点击获取你的家园扭蛋</Link>) : <div></div>}
       </div>
     </div>
