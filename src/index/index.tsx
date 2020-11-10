30223456ee35ba74bf66b9cd430498b749ce4ad2
z@wood <2511841570@qq.com>
2020-11-10 21:25:03 +0800

    :art: 使用ref代替直接操作dom

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -1,52 +1,33 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './index.scss';
 import {Link} from 'react-router-dom';
 import Loading from '../components/loading';
 
-const Words: React.FC = () => {
-  return (
-    <div className="index-words">
-      <div id="sentence1">
-        你即将看到的是<span style={{ fontSize: "6.5vw" }}>家园扭蛋机</span>
-        <p style={{ fontSize: "6.3vw" }}>NCUHOME’S GACHAPON</p>
-      </div>
-      <div id="sentence2">
-        正如它的英文名 (咔擦砰)
-        <p><span style={{ fontSize: "8vw", marginLeft: 20 }}>"咔擦"</span>一声</p>
-      </div>
-      <div id="sentence3">
-        <p>2019年</p>
-        <p>那些或许被你忽视的小碎片</p>
-        <p>就会<span style={{ fontSize: "8vw" }}>"砰"</span>的涌现</p>
-        <p>月光下的足迹</p>
-        <p>教室里的身影</p>
-        <p>一些无意坚持下来的小习惯...</p>
-      </div>
-      <div id="sentence4">
-        <p>把它们收集起来</p>
-        <p>一起来拼出你的校园人设</p>
-        <p style={{ fontSize: "6.5vw" }}>惊喜在这里等你...</p>
-      </div>
-    </div>
-  )
-}
 const Index: React.FC = () => {
   const [flag, setFlag] = useState<boolean>(true);
   const [show, changeShow] = useState<boolean>(false);
 
-  window.onload = () => {
-    const loadingEle: HTMLElement | null = document.getElementById("loading")
+  const ref1 = useRef<any>()
+  const ref2 = useRef<any>()
+  const ref3 = useRef<any>()
+  const ref4 = useRef<any>()
+  const loadingRef = useRef<any>()
 
-    // 怎么解决loadingEle为null
+  window.onload = () => {
+    const loadingEle: HTMLElement = loadingRef.current
     loadingEle.style.display = "none"
+  }
+
+  const isIOS = (): boolean => {
+    const userAgent = navigator.userAgent;
+    const isIOS: boolean = !!userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    return isIOS
   }
   // 通过锚点实现点击滑到底部
   const toBottom = (): void => {
-    const userAgent = navigator.userAgent;
-    const isIOS: boolean = !!userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
     const ele:HTMLElement = document.body;
     // safari不支持scrollIntoView的options
-    if (isIOS) {
+    if (isIOS()) {
       ele.scrollIntoView(false);
     } else {
       ele.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
@@ -56,12 +37,13 @@ const Index: React.FC = () => {
       showWords(0)
     }
   }
+
   const showWords = (count: number): void => {
     setFlag(false)
-    const s1: HTMLElement | null = document.getElementById("sentence1");
-    const s2: HTMLElement | null = document.getElementById("sentence2");
-    const s3: HTMLElement | null = document.getElementById("sentence3");
-    const s4: HTMLElement | null = document.getElementById("sentence4");
+    const s1: HTMLElement = ref1.current
+    const s2: HTMLElement = ref2.current
+    const s3: HTMLElement = ref3.current
+    const s4: HTMLElement = ref4.current
     // 保证每个dom节点都拿到,否则一直执行回调函数
     if (s1 && s2 && s3 && s4) {
       if (count < 3) {
@@ -87,9 +69,10 @@ const Index: React.FC = () => {
       showWords(count);
     }
   }
+
   return (
     <div>
-      <div id="loading"><Loading /></div>
+      <div id="loading" ref={loadingRef}><Loading /></div>
       <div className="index-container" id="container" onClick={toBottom}>
         <div className="index-title">2019
           <span className="index-flag">
@@ -97,7 +80,29 @@ const Index: React.FC = () => {
           </span>
           <div style={{fontSize: "6vw"}}>(任意点击进入扭蛋机)</div>
         </div>
-        <Words />
+        <div className="index-words">
+          <div id="sentence1" ref={ref1}>
+            你即将看到的是<span style={{ fontSize: "6.5vw" }}>家园扭蛋机</span>
+            <p style={{ fontSize: "6.3vw" }}>NCUHOME’S GACHAPON</p>
+          </div>
+          <div id="sentence2" ref={ref2}>
+            正如它的英文名 (咔擦砰)
+            <p><span style={{ fontSize: "8vw", marginLeft: 20 }}>"咔擦"</span>一声</p>
+          </div>
+          <div id="sentence3" ref={ref3}>
+            <p>2019年</p>
+            <p>那些或许被你忽视的小碎片</p>
+            <p>就会<span style={{ fontSize: "8vw" }}>"砰"</span>的涌现</p>
+            <p>月光下的足迹</p>
+            <p>教室里的身影</p>
+            <p>一些无意坚持下来的小习惯...</p>
+          </div>
+          <div id="sentence4" ref={ref4}>
+            <p>把它们收集起来</p>
+            <p>一起来拼出你的校园人设</p>
+            <p style={{ fontSize: "6.5vw" }}>惊喜在这里等你...</p>
+          </div>
+        </div>
         {show ? (<Link to="/papers" className="index-get">点击获取你的家园扭蛋</Link>) : <div></div>}
       </div>
     </div>
