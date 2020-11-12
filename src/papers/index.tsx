e1e907665b6f88fa4a3f8b01022d784bf2f2b5e8
z@wood <2511841570@qq.com>
2020-11-12 09:50:01 +0800

    :hankey: 利用缓存代替canvas

--- src/papers/index.tsx
+++ src/papers/index.tsx
@@ -1,40 +1,25 @@
-import React, {useRef, useEffect, useReducer, useState} from 'react';
-import SuperGif from '../utils/gifControl/libgif';
+import React, {useRef, useEffect, useState} from 'react';
 import Loading from '../components/loading'
 import './index.scss'
 
-interface IAction {
-  type: string,
-  data: any
-}
 const Papers: React.FC = () => {
-  const reducer = (state: any, action: IAction) => {
-    if (action.type === 'change') {
-      return action.data
+  const [loading, setLoading] = useState<boolean>(false)
+  const imgRef = useRef<any>()
+  const change = () => {
+    setLoading(true)
+    imgRef.current.src = "http://antelope.fun/nd.gif"
+    imgRef.current.onload = () => {
+      setLoading(false)
     }
   }
-  const [loading, setLoading] = useState<boolean>(true)
-  const [sup, dispatch] = useReducer<any>(reducer, null) 
-  const imgRef = useRef<any>()
-  const test = () => {
-    sup.play()
-    setTimeout(() => {
-      sup.pause()
-    }, 3700)
-  }
   useEffect(() => {
-    let sup1 = SuperGif({gif: imgRef.current})
-    dispatch({type: 'change', data: sup1})
-    sup1.load(() => {
-      setLoading(false)
-    })
   }, [])
 
   return (
-    <div style={{ width: "100vw", height: "100vh"}}>
-      {/* {loading ? <Loading /> : ""} */}
+    <div style={{ width: "100vw", height: "100vh", position: "fixed" }}>
+      {loading ? <Loading /> : ""}
       <div>
-        <img ref={imgRef} className="paper-container" src="http://antelope.fun/paperbg.jpg" id="http://antelope.fun/nd.gif" alt="0" />
+        <img ref={imgRef} className="paper-container" src="http://antelope.fun/paperbg.jpg" onClick={change} />
       </div>
     </div>
   )
